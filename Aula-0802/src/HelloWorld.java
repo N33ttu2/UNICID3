96a22f6d06d624006517be5e5e8e3f1d988824af
N33ttu2 <90645003+N33ttu2@users.noreply.github.com>
2024-02-08 10:47:10 -0300

    Add files via upload

---
+++ Aula-0802/src/HelloWorld.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		//void metodo sem retorno
+		//Letra maiuscula (em Java) = classe
+		System.out.println("Hello World");
+		int a = 10;
+		int b = 20;
+		int soma = (a + b);
+		System.out.println("Resultado = " + soma);
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
