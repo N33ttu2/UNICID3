df7086cf3a8f44bfea2470bff579f47c35e11bd5
N33ttu2 <90645003+N33ttu2@users.noreply.github.com>
2024-02-20 10:43:45 -0300

    Add files via upload

---
+++ K7/busca.py
@@ -0,0 +1,20 @@
+def busca (lista, chave):
+    for i, elemento in enumerate(lista):
+        if elemento == chave:
+            return i # = chave encontrada
+    return -1 # = chave n encontrada
+    # Acima -> algorítmo da busca
+    # Abaixo -> MAIN
+def main():
+    lista = [20, 5, 15, 24, 67, 45, 1, 76, 21, 11]
+    chave = 45
+
+    posicao = busca(lista, chave)
+
+    if posicao != -1: 
+        print(f"Posição da chave {chave} na lista: {posicao}")
+    else:
+        print(f"A chave {chave} não se encontra na lista.")
+#MAIN para executar o algorítmo com a busca.
+if __name__ == "__main__":
+    main()
