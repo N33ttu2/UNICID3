ebd54b1923e7465aada368c0af5b69d83337d5ae
João  Bispo de Santana Neto <90645003+N33ttu2@users.noreply.github.com>
2024-11-19 10:49:53 -0300

    aula19/11 hash

---
+++ hash.py
@@ -0,0 +1,25 @@
+#digamos que  queremos distribuir os seguintes numeros em uma tabela hash
+#12, 44, 71, 99, 23, 8, 121, 133, 168, 144, 171, 111, 16
+
+#Criar: For e estrutura hash
+def espalhamentoInserc(valor):
+    if (valor % 2) == 0:
+        lista0.append(valor)
+    else:
+        lista1.append(valor)
+vetor = [12, 44, 71, 99, 23, 8, 121, 133, 168, 144, 171, 111, 16]
+
+# Qual o valor de M? neste caso M == 2
+# Lista Hash dividida em duas partes
+lista0 = []
+lista1 = []
+
+listaHash = [lista0, lista1] # <- vetor vazio dinamicamente alocado, composta de outras listas
+
+for item in vetor:
+    espalhamentoInserc(item)
+
+print(listaHash)
+
+#Estrutura do hash ^^^^
+#Inserir valor dentro da lista
